226d58bcdb1fc8213183ece33585b6c676cc9dc8
JovitaBeliak <jovitabeliak@gmail.com>
2026-01-29 15:43:06 +0200

    4 pritaikytas puslapio dizainas, sukurtas static katalogas ir idetas css

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('autoservice.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
