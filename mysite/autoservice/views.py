9848ea5d42994a9b781315c71c519adf4396f42d
JovitaBeliak <jovitabeliak@gmail.com>
2026-02-09 11:07:28 +0200

    8 pataisyta pamoka: mano uzsakymuose prideta nuoroda i uzsakymo detalu paslapi

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -64,7 +64,7 @@ def search(request):
 class MyOrdersListView(LoginRequiredMixin, generic.ListView):
     model = Order
     template_name = 'my_orders.html'
-    context_object_name = 'myorders'
+    context_object_name = 'orders'
 
     def get_queryset(self):
         return Order.objects.filter(client=self.request.user)
